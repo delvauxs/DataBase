0f0bfce231f42fe5b87efd4348d8d521fbc25d18
delvauxs <shirleydelvaux@gmail.com>
2016-06-12 14:33:47 +0200

    Revert "All the database schema studies"
    This reverts commit c6210333b80583a10fc33ea2cbf84564d0b8ae5e.

--- Genererpäie.sql
+++
@@ -1,7 +0,0 @@
-CREATE FUNCTION
-  GenererPaie
-  /* Routine name */
-  ()                            /* An empty parameter list */
-  RETURNS DECIMAL(3,2)          /* What the function returns */
-  CONTAINS SQL                 /* Data access clause */
-  RETURN 3.14;
